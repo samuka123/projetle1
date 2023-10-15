bfc11572dc5dfe0d96ba96a0dc5ff402dc2665c0
SamuelESIEE <samuel.pereiracarreira@edu.esiee.fr>
2023-10-15 22:38:49 +0200

    integration firebase in gradle

--- app/src/main/java/com/example/projetle1/MainActivity.java
+++ app/src/main/java/com/example/projetle1/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,5 +14,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-
-//test nelson 22
+//Sam
