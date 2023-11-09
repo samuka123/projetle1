0402ec79eec76ba9da0b42c543633101e45d1890
SamuelESIEE <samuel.pereiracarreira@edu.esiee.fr>
2023-11-09 17:26:16 +0100

    creation login and inscription activity & syn firebase

--- app/src/main/java/com/example/projetle1/Inscription.java
+++ app/src/main/java/com/example/projetle1/Inscription.java
@@ -2,20 +2,13 @@ package com.example.projetle1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
-
-
-    private static final String TAG = "MainActivity";
-
+public class Inscription extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_inscription);
     }
 }
