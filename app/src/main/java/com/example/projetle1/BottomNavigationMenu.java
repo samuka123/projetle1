bbcec407d70dc85529cb9484c29476f455cc6ed5
SamuelESIEE <samuel.pereiracarreira@edu.esiee.fr>
2023-11-14 22:50:06 +0100

    Change of textView to activity on the bottom menu

--- app/src/main/java/com/example/projetle1/BottomNavigationMenu.java
+++ app/src/main/java/com/example/projetle1/BottomNavigationMenu.java
@@ -1,23 +1,27 @@
 package com.example.projetle1;
+
 import android.app.Activity;
-import android.widget.TextView;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class BottomNavigationMenu {
-    public static void setupBottomMenu(BottomNavigationView bottomNavigationView, final Activity activity, TextView tvContenName) {
+public class BottomNavigationMenu extends AppCompatActivity {
+    public static void setupBottomMenu(BottomNavigationView bottomNavigationView, final Activity activity) {
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             int itemId = item.getItemId();
 
             if (itemId == R.id.home) {
-                tvContenName.setText(R.string.tv_str_content_home);
+                Intent intent = new Intent(activity, PagePrincipale.class);
+                activity.startActivity(intent);
+                activity.finish();
                 return true;
             } else
                 if (itemId == R.id.account) {
-                    tvContenName.setText(R.string.tv_str_account);
                 return true;
             } else
-                if(itemId == R.id.message){
-                tvContenName.setText(R.string.tv_str_message);
+                if(itemId == R.id.message) {
                     return true;
                 }
             return false;
