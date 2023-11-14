0ad15869645727d6e7319b4d2d3e4191b4f70ff2
Nelson <nelson.kessy@edu.esiee.f>
2023-11-14 22:43:03 +0100

    inscription layout

--- app/src/main/java/com/example/projetle1/MainActivity.java
+++ app/src/main/java/com/example/projetle1/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+WQ                                                                                                                                          setContentView(R.layout.inscription);
 
 
         auth = FirebaseAuth.getInstance();
