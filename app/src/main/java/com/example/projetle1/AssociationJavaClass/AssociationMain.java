06db5c2b1d86c6a98264a7d78818a7046eaacb78
zhouh <zhouhaozhanga@gmail.com>
2023-11-26 16:26:38 +0100

    création d'une nouvelle page par contre je ne sais pas les code pour le faire fonctionner

--- app/src/main/java/com/example/projetle1/AssociationJavaClass/AssociationMain.java
+++ app/src/main/java/com/example/projetle1/AssociationJavaClass/AssociationMain.java
@@ -18,15 +18,15 @@ public class AssociationMain extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_association_list);
+       // setContentView(R.layout.activity_association_list);
 
         List<AssociationItem> vAssociationArrayList = new ArrayList<>();
         vAssociationArrayList.add(new AssociationItem("Action contre la faim", "https://donner.actioncontrelafaim.org/agir/~mon-don?source=bouton-site&medium=menu"));
         vAssociationArrayList.add(new AssociationItem("Medecins sans frontieres", "https://soutenir.msf.fr/faire-un-don/~mon-don"));
         vAssociationArrayList.add(new AssociationItem("Les restos du coeur", "https://dons.restosducoeur.org/particulier/~mon-don?gad_source=1&gclid=CjwKCAiAgeeqBhBAEiwAoDDhn56UOzfol7mVpt9WAWHs7AJtkGmySe0i741oc1ftfA0RqDLFfhH_VhoCDmcQAvD_BwE"));
 
-        ListView vAssociationList = findViewById(R.id.association_list_id);
-        vAssociationList.setAdapter(new AssociationItemAdapter(this, vAssociationArrayList));
+        //ListView vAssociationList = findViewById(R.id.association_list_id);
+        //vAssociationList.setAdapter(new AssociationItemAdapter(this, vAssociationArrayList));
 
     }
 
