2974e6b8c6ce56cf03316fb04c0aaab3a64d78a3
SamuelESIEE <samuel.pereiracarreira@edu.esiee.fr>
2023-12-01 02:19:59 +0100

    implementation of profil & CompteFragment

---
+++ app/src/main/java/com/example/projetle1/All_Utilisateur.java
@@ -0,0 +1,56 @@
+package com.example.projetle1;
+
+public class All_Utilisateur {
+
+
+
+
+    String prenom,nom,email, adresse, uid,url;
+
+    public All_Utilisateur(){}
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
